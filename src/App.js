d84b0a3938d71b9e2b3bd108d67c7178aa7257b6
michaelrlai8 <michaelrlai8@gmail.com>
2022-06-12 23:35:31 -0700

    added button functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import "./index.css";
-import { useState } from "react";
+import React from "react";
+import axios from "axios";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
 
-  const incr = () => {
-    setCount(count + 1);
+  React.useEffect(() => {
+    const getCount = async () => {
+      try {
+        const response = await axios.get("/api");
+        setCount(response.data.rows[0].count);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCount();
+  }, []);
+
+  const incr = async () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    try {
+      const response = await axios({
+        method: "put",
+        url: "/api/change",
+        data: {
+          count: newCount,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const decr = () => {
-    setCount(count - 1);
+  const decr = async () => {
+    const newCount = count - 1;
+    setCount(newCount);
+    try {
+      const response = await axios({
+        method: "put",
+        url: "/api/change",
+        data: {
+          count: newCount,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const reset = () => {
-    setCount(0);
+  const reset = async () => {
+    const newCount = 0;
+    setCount(newCount);
+    try {
+      const response = await axios({
+        method: "put",
+        url: "/api/change",
+        data: {
+          count: newCount,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
